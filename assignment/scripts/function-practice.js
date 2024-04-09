0d3d2c626f5600d901223089f083b91407c12dab
Amber Schaack <aschaack9@gmail.com>
2024-04-09 13:20:08 -0500

    completed stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -17,6 +17,7 @@ function helloName(name = 'stranger') {
   return `Hello, ${name}!`;
 }
 // Remember to call the function to test
+console.log('in helloName function:');
 console.log(helloName('Amber'));
 console.log(helloName());
 // Added a default to 'name' so that undefined is not returned if a name is not given.
@@ -25,6 +26,7 @@ console.log(helloName());
 function addNumbers(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
+console.log('in addNumbers function:');
 console.log(addNumbers(3, 5));
 console.log(addNumbers(145, 732));
 console.log(addNumbers(0.478, 976));
@@ -33,6 +35,7 @@ console.log(addNumbers(0.478, 976));
 function multiplyThree(num1, num2, num3) {
   return num1 * num2 * num3;
 }
+console.log('in multiplyThree function:');
 console.log(multiplyThree(3, 7, 10));
 console.log(multiplyThree(3.6, 7.2, 10.5));
 console.log(multiplyThree(-3, 75, 9.3));
@@ -48,6 +51,7 @@ function isPositive(number) {
 }
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
+console.log('in isPositive function:');
 console.log(isPositive(5));
 console.log(isPositive(-5));
 
@@ -59,6 +63,7 @@ function getLast(array) {
   }
   return array[array.length - 1];
 }
+console.log('in getLast function:');
 let drinksArray = ['water', 'pop', 'juice'];
 console.log(getLast(drinksArray));
 let emptyArray = [];
@@ -71,34 +76,59 @@ function find(value, array) {
   for (i = 0; i < array.length; i++) {
     if (value === array[i]) {
       return true;
-    } else {
-      return false;
     }
   }
+  return false;
 }
-let shoesArray = ['boots', 'tennis', 'runners'];
-console.log(find('boots', shoesArray));
-console.log(find('slippers', shoesArray));
+console.log('in find function:');
+let numbersOne = [5, 7, 9, 2];
+console.log(find(7, numbersOne));
+console.log(find(4, numbersOne));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {}
+
+function isFirstLetter(letter, string) {
+  if (string[0] === letter) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('in isFirstLetter function:');
+let string1 = 'welcome to the jungle';
+console.log(isFirstLetter('w', string1));
+console.log(isFirstLetter('t', string1));
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll(array) {
   let sum = 0;
   // TODO: loop to add items
-
+  for (i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
   // TODO: return the sum
+  return sum;
 }
+console.log('in sumAll function:');
+let array1 = [1, 2, 3, 4];
+let array2 = [5, 6, 7, 8];
+console.log(sumAll(array1));
+console.log(sumAll(array2));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-function allPositive() {}
+function allPositive(array) {
+  const positives = array.filter((num) => num > 0);
+  return positives;
+}
+console.log('in allPositive function:');
+let numbersArray = [-1, 0, 1, 2, -5, 7];
+console.log(allPositive(numbersArray));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
